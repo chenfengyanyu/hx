1229f7f81b999e15ffcc5a2fa2e0afa31d9d08f9
Jartto <jartto@JarttodeiMac.lan>
2022-07-05 09:14:21 +0800

    feat: add header

--- hx3/src/App.vue
+++ hx3/src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <Header />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
